242776e569c630d70e92e63c294d16f679402f3d
Vitalii Protsko <provitaleks@gmail.com>
2024-03-19 17:26:24 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const App = () => {
-  const [values, setValues] = useState({
-    x: 0,
-    y: 0,
-  });
+  // const [feedbacks, setValues] = useState({
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // });
 
-  const updateX = () => {
-    setValues({
-      ...values,
-      x: values.x + 1,
-    });
-  };
-
-  const updateY = () => {
-    setValues({
-      ...values,
-      y: values.y + 1,
-    });
+  const updateFeedback = (feedbackType) => {
+    console.log("Hello:", feedbackType);
+    // setValues({
+    //   ...feedbacks,
+    //   feedbackType: feedbacks.feedbackType + 1,
+    // });
   };
 
   return (
-    <div>
-      <p>
-        x: {values.x}, y: {values.y}
-      </p>
-
-      <button onClick={updateX}>Update x</button>
-      <button onClick={updateY}>Update y</button>
-    </div>
+    <>
+      <button onClick={"Hello"}>Good</button>
+      <button onClick={"Privet"}>Neutral</button>
+      <button onClick={"rtf"}>Bad</button>
+    </>
   );
 };
 
